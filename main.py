7744e5e07c0c0abf21d793f6203a3a3199d821c9
Pomohorus <namoff95@mail.ru>
2022-06-19 09:51:30 +0300

    done 2

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import datetime
 import logging
 
-
-def new_gen(old_fun):
-    def new_fun(n):
-        print('Дата и время: ', datetime.datetime.now())
-        print('Имя функции: ', old_fun)
-        print('Аргументы при вызове: ', n)
-        print('Возвращаемое значение: ')
-        result = old_fun(n)
-        logging.basicConfig(filename='exlog.log', level=logging.INFO)
-        logging.info('writiing in the log')
-        return result
-    return new_fun
-
-@new_gen
-def flat_generator(n):
-    for item in n:
-        for i in item:
-            yield i
+def par_der(nested):
+    def new_gen(old_fun):
+        def new_fun(*args, **kwars):
+            print('Дата и время: ', datetime.datetime.now())
+            print('Имя функции: ', old_fun)
+            print('Аргументы при вызове: ', nested)
+            print('Возвращаемое значение: ')
+            result = old_fun(nested)
+            logging.basicConfig(filename='exlog.log', level=logging.INFO)
+            logging.info('writiing in the log')
+            return result
+        return new_fun
+    return new_gen
 
 nested_list = [
     ['a', 'b', 'c'],
@@ -26,5 +21,13 @@ nested_list = [
     [1, 2, None],
 ]
 
-for item in flat_generator(nested_list):
+@par_der(nested=nested_list)
+def flat_generator(n):
+    for item in n:
+        for i in item:
+            yield i
+
+
+
+for item in flat_generator():
     print(item)
